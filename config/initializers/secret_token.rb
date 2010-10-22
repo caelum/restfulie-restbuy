bb80aa4c00b0ee65fc6c8e3089bb84fa2ff776a1
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-22 09:31:34 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restbuy::Application.config.secret_token = '706fb6dbcc30ef9a431f3d7f9214e1807d72fa152e68a0ae76cee6e058049eaa66a384632fd500d9e2803ebb2e8d7dce60b665fcf0abe39ba73665919206e2c1'
