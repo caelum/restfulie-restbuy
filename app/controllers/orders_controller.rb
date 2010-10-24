e12ac0a6735102966058fbc5d60cf18e0185a0a0
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-24 11:36:00 -0200

    removing show orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,4 @@
 class OrdersController < ApplicationController
-  # GET /orders
-  # GET /orders.xml
-  def index
-    @orders = Order.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @orders }
-    end
-  end
 
   # GET /orders/1
   # GET /orders/1.xml
