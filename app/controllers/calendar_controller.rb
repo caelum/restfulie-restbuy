c6b3ff1699596a73016907e840bf1265c6aaac12
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-29 12:20:16 -0200

    link header comment

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -8,6 +8,17 @@ class CalendarController < ApplicationController
   def show
     @order = Order.find(params[:order_id])
     
+    links = []
+    links << ["", order_url(@order)]
+    links << ["payments", order_payments_url(@order)]
+    links << ["tracking", "http://your_tracking_uri"]
+    links << ["cancel", order_url(@order)]
+    def links.to_s
+      inject("") do |content, link|
+        content + "\n<a href=\"#{link[1]}\" rel=\"#{link[0]}\">#{link[0]}</a>"
+      end
+    end
+    
     cal = Calendar.new
       order = @order
       @order.items.each do |item|
@@ -15,7 +26,7 @@ class CalendarController < ApplicationController
         dtstart       Date.new(2010, 11, 05)
         dtend         Date.new(2010, 11, 06)
         summary     "Delivery #{item.product.name}."
-        description "The product should be delivered today."
+        description "The product should be delivered today. #{links.to_s}"
         klass       "PRIVATE"
       end
     end
