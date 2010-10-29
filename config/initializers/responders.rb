331127ad4e8e901739ea1f7c1597c48305cd7911
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-29 11:04:44 -0200

    created trait already exists

--- config/initializers/responders.rb
+++ config/initializers/responders.rb
@@ -1,11 +1,1 @@
 require 'respondie'
-
-module Restfulie::Server::ActionController::Trait::Created
-  def to_format
-    if (options[:status] == 201) || (options[:status] == :created)
-      render :status => 201, :location => controller.url_for(resource), :text => ""
-    else
-      super
-    end
-  end
-end
